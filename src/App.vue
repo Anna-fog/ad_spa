d73932f2ebbd24ef5fc053768005ad7f1cc0e64b
anna <anna49.fog@yandex.ru>
2021-03-30 17:23:28 +0300

    show error in case of incorrect password

--- src/App.vue
+++ src/App.vue
@@ -42,6 +42,22 @@
     <v-content>
       <router-view></router-view>
     </v-content>
+
+    <template v-if="error">
+      <v-snackbar
+        :timeout="5000"
+        :multi-line="true"
+        color="error"
+        @input="closeError"
+        :value="true"
+      >
+        {{error}}
+        <v-btn dark flat @click.native="closeError">
+          Close
+        </v-btn>
+      </v-snackbar>
+    </template>
+
   </v-app>
 </template>
 
@@ -57,6 +73,16 @@ export default {
         {title: 'New ad', icon: 'note_add', url: '/new'},
         {title: 'My ads', icon: 'list', url: '/list'}
       ]
+    }
+  },
+  computed: {
+    error () {
+      return this.$store.getters.error
+    }
+  },
+  methods: {
+    closeError () {
+      this.$store.dispatch('clearError')
     }
   }
 
