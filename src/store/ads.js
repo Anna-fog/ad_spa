05eec2d52898813e7779278a5b46756b5b6e6a46
anna <anna49.fog@yandex.ru>
2021-04-06 11:03:12 +0300

    add edit ad modal

--- src/store/ads.js
+++ src/store/ads.js
@@ -14,30 +14,6 @@ class Ad {
 export default {
   state: {
     ads: []
-    // ads: [
-    //   {
-    //     title: 'First ad',
-    //     description: 'ad description',
-    //     promo: false,
-    //     imageSrc: 'https://cdn.vuetifyjs.com/images/carousel/sky.jpg',
-    //     id: '123'
-    //   },
-    //   {
-    //     title: 'Second ad',
-    //     description: 'ad description',
-    //     promo: true,
-    //     imageSrc: 'https://cdn.vuetifyjs.com/images/carousel/squirrel.jpg',
-    //     id: '1234'
-    //   },
-    //   {
-    //     title: 'Third ad',
-    //     description: 'ad description',
-    //     promo: true,
-    //     imageSrc: 'https://cdn.vuetifyjs.com/images/carousel/bird.jpg',
-    //     id: '12345'
-    //   }
-    // ]
-
   },
   mutations: {
     createAd (state, payload) {
